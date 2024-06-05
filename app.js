71ef925fc78aee390662c6e2f505864790386818
gds90 <you@example.com>
2024-06-05 17:11:11 +0200

    Aggiunte e testate operazioni CRUD su post

--- app.js
+++ app.js
@@ -8,6 +8,54 @@ const {
     createTags
 } = require('./utils/crud-tag.js');
 
+const { createPost,
+    readPostBySlug,
+    readAllPosts,
+    updatePost,
+    deletePost
+} = require('./utils/crud-post.js');
+
+// Creazione post
+// createPost({
+//     title: 'Post di prova 5',
+//     slug: 'post-di-prova-5',
+//     image: 'https://photo/prova5',
+//     content: 'Contenuto del post di prova 5',
+//     published: true,
+//     tags: {
+//         connect: [
+//             { id: 5 },
+//             { id: 6 }
+//         ]
+//     },
+//     category: {
+//         connect: { id: 4 },
+//     }
+// }, newPost => console.log(newPost));
+
+// Lettura post tramite slug 
+// readPostBySlug('post-di-prova-4', post => console.log(post));
+
+// Lettura di tutti i post 
+// readAllPosts(posts => console.log(posts));
+
+// Aggiornamento di un post (aggiunta di categoria e tags)
+// updatePost('post-di-prova-3', {
+//     category: {
+//         set: [
+//             { id: 1 }
+//         ]
+//     },
+//     tags: {
+//         set: [
+//             { id: 2 }
+//         ]
+//     }
+// }, updatedPost => console.log('Post aggiornato correttamente: ', updatedPost));
+
+// ELiminazione di un post
+// deletePost('post-di-prova-3');
+
 // Creazione categorie
 // createCategories(['Cucina', 'Sport', 'Tecnologia'], count => console.log(count));
 // createCategory({ name: 'Musica' }, newCategory => console.log('Nuova categoria inserita: ', newCategory));
---
+++ utils/crud-post.js
@@ -0,0 +1,62 @@
+const { PrismaClient } = require('@prisma/client');
+const prisma = new PrismaClient();
+
+// Il Post deve contenere le seguenti proprietà:
+// title
+// slug (deve essere univoco)
+// image (non obbligatoria)
+// content
+// published (boolean)
+
+// implementate tutte le operazioni CRUD per il modello Post, ovvero:
+// Una funzione che consente di creare un Post.
+const createPost = (data, callback) => {
+    prisma.post.create({ data })
+        .then(newPost => callback(newPost))
+        .catch(error => console.error(error));
+}
+
+// Una funzione che permette di leggere un Post usando lo slug.
+const readPostBySlug = (slug, callback) => {
+    prisma.post.findUnique({
+        where: { slug },
+        include: {
+            tags: {
+                select: {
+                    name: true
+                }
+            },
+            category: true,
+        }
+    })
+        .then(post => callback(post))
+        .catch(error => console.error(error));
+}
+
+// Una funzione che restituisce l'elenco di tutti i Post.
+const readAllPosts = (callback) => {
+    prisma.post.findMany()
+        .then(posts => callback(posts))
+        .catch(error => console.error(error));
+}
+// Una funzione che consente di modificare un Post.
+const updatePost = (slug, data, callback) => {
+    prisma.post.update({ where: { slug }, data })
+        .then(updatedPost => callback(updatedPost))
+        .catch(error => console.error(error));
+}
+
+// Una funzione che elimina un Post.
+const deletePost = (slug) => {
+    prisma.post.delete({ where: { slug } })
+        .then(() => console.log(`Post con slug ${slug} eliminato`))
+        .catch(error => console.error(error));
+}
+
+module.exports = {
+    createPost,
+    readPostBySlug,
+    readAllPosts,
+    updatePost,
+    deletePost
+}
